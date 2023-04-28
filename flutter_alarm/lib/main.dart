2d61b8c93a8c640dcda66c2860627ae4594cc69d
Kenan Cosic <45003119+Nokee221@users.noreply.github.com>
2023-04-28 10:38:41 +0200

    init

---
+++ flutter_alarm/lib/main.dart
@@ -0,0 +1,150 @@
+import 'dart:developer' as developer;
+import 'dart:isolate';
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+
+/// The [SharedPreferences] key to access the alarm fire count.
+const String countKey = 'count';
+
+/// The name associated with the UI isolate's [SendPort].
+const String isolateName = 'isolate';
+
+/// A port used to communicate from a background isolate to the UI isolate.
+ReceivePort port = ReceivePort();
+
+/// Global [SharedPreferences] object.
+SharedPreferences? prefs;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Register the UI isolate's SendPort to allow for communication from the
+  // background isolate.
+  IsolateNameServer.registerPortWithName(
+    port.sendPort,
+    isolateName,
+  );
+  prefs = await SharedPreferences.getInstance();
+  if (!prefs!.containsKey(countKey)) {
+    await prefs!.setInt(countKey, 0);
+  }
+
+  runApp(const AlarmManagerExampleApp());
+}
+
+/// Example app for Espresso plugin.
+class AlarmManagerExampleApp extends StatelessWidget {
+  const AlarmManagerExampleApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: _AlarmHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class _AlarmHomePage extends StatefulWidget {
+  const _AlarmHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _AlarmHomePageState createState() => _AlarmHomePageState();
+}
+
+class _AlarmHomePageState extends State<_AlarmHomePage> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    AndroidAlarmManager.initialize();
+  }
+
+  // The background
+  static SendPort? uiSendPort;
+
+  // The callback for our alarm
+  @pragma('vm:entry-point')
+  static Future<void> callback() async {
+    developer.log('Alarm aktiviran u ' + DateTime.now().toString());
+    // Get the previous cached count and increment it.
+    final prefs = await SharedPreferences.getInstance();
+    final currentCount = prefs.getInt(countKey) ?? 0;
+    await prefs.setInt(countKey, currentCount + 1);
+
+    // This will be null if we're running in the background.
+    uiSendPort ??= IsolateNameServer.lookupPortByName(isolateName);
+    uiSendPort?.send(null);
+
+    await showNotification('Periodicna obavijest');
+  }
+
+  static Future<void> showNotification(String text) async {
+    const channelId = 'alarm_channel';
+    const channelName = 'Alarm';
+    const channelDescription = 'Alarm channel';
+
+    final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    const initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    final initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    const androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        channelId, channelName,
+        importance: Importance.max, priority: Priority.high, ticker: 'ticker');
+    const platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Obavijest',
+      text,
+      platformChannelSpecifics,
+      payload: 'Custom_Sound',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme.headlineMedium;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              key: const ValueKey('RegisterOneShotAlarm'),
+              onPressed: () async {
+                await AndroidAlarmManager.oneShot(
+                  const Duration(seconds: 5),
+                  // Ensure we have a unique alarm ID.
+                  0,
+                  callback,
+                  exact: true,
+                  wakeup: true,
+                );
+              },
+              child: const Text(
+                'Zakazi alaram',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
